c48c68647436df4f1b03191c6bf2c50aca53c8e3
Brittany Ellich <brittanyellich@github.com>
2024-03-24 21:10:53 -0700

    Add skip for folks on leave and check that a next one-on-one is scheduled

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Indexes need to match whatever sheet you are using
 var emailIndex = 0; /* The email of the individual you are managing 1:1s with */
-var cadenceIndex = 6; /* The desired cadence, see cadenceDictionary below */
-var lastOneonOnIndex = 9; /* The date you had your last one on one */
-var lastOneonOneThreshold = 10; /* The column where the script will flag whether that date is "Good", "Close", "Overdue", 
+var cadenceIndex = 7; /* The desired cadence, see cadenceDictionary below */
+var lastOneonOnIndex = 8; /* The date you had your last one on one */
+var lastOneonOneThreshold = 13; /* The column where the script will flag whether that date is "Good", "Close", "Overdue", 
                                 governed by the offset periods in cadenceDictionary */
-var lastMeetingIndex = 12; /* The date of any last meeting in which you and the individual were in today */
-var lastMeetingTitleIndex = 13; /* The title of that last meeting */
-var emailOfCalendarOwner = 'your@email.com'; /* The owner of the calendar */
+var lastMeetingIndex = 9; /* The date of any last meeting in which you and the individual were in today */
+var lastMeetingTitleIndex = 10; /* The title of that last meeting */
+var onLeaveUntilIndex = 11; /* Date until someone is on leave, if set and is in the future, don't set time as overdue */
+var nextOneonOneIndex = 12; /* Date of the next one on one */
+var emailOfCalendarOwner = 'name@email.com'; /* The owner of the calendar */
  
 // Total Period Days From Today represents the entire duration of the cadence, Yellow Offset from End of Period, governs "Close"
 var cadenceDictionary = {
@@ -19,6 +21,11 @@ var cadenceDictionary = {
   "Quarterly": [90, 14],
   "Semi-Annually": [180, 21]
 };
+
+var NA = "n/a";
+var GOOD = "Good";
+var CLOSE = "Close";
+var OVERDUE = "Overdue";
  
 // This is the function you put into your sheet, attached to an image. 
 function updateMeetingData() {
@@ -39,6 +46,7 @@ function updateMeetingData() {
   for (var rowIndex = 1; rowIndex < lastRowIndex; rowIndex++) {
     var currentEmail = (values[rowIndex][emailIndex]).toLowerCase();
     rowDictionaryByEmail[currentEmail] = values[rowIndex];
+    rowDictionaryByEmail[currentEmail].rowIndex = rowIndex;
   }
  
   // Process meetings from the last 6 months on the calendar, can adjust for shorter/longer depending on what you want
@@ -98,11 +106,19 @@ function updateMeetingData() {
     var cadence = values[rowIndex][cadenceIndex];
     var lastOneOnOneDate = values[rowIndex][lastOneonOnIndex];
 
+    // If a person is on leave, skip them and mark them as n/a
+    var onLeaveDate = data.getCell(rowIndex + 1, onLeaveUntilIndex + 1).getValue();
+    if (onLeaveDate && onLeaveDate > now) {
+      data.getCell(rowIndex + 1, lastOneonOnIndex + 1).setBackgroundRGB(255, 255, 255);
+      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue(NA);
+      continue;
+    }
+
     // If there is no meeting data for this person, clear the column
     if (lastOneOnOneDate == null || lastOneOnOneDate == "" || cadence == null || cadence == "") {
-      // Clear
-      data.getCell(rowIndex + 1, lastOneonOnIndex + 1).setBackgroundRGB(255, 255, 255);
-      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue("n/a");
+      // Set Overdue, one-on-one is needed for this person
+      data.getCell(rowIndex + 1, lastOneonOnIndex + 1).setBackgroundRGB(230, 160, 145);
+      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue(OVERDUE);
       continue;
     }
 
@@ -116,18 +132,82 @@ function updateMeetingData() {
     if (lastOneOnOneDate >= firstDateForYellow) {
       // Green
       data.getCell(rowIndex + 1, lastOneonOnIndex + 1).setBackgroundRGB(212, 250, 200);
-      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue("Good");
+      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue(GOOD);
     }
     else if (lastOneOnOneDate > lastDateOfPeriod) {
       // Yellow
       data.getCell(rowIndex + 1, lastOneonOnIndex + 1).setBackgroundRGB(232, 235, 52);
-      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue("Close");
+      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue(CLOSE);
     }
     else
     {
       // Red
       data.getCell(rowIndex + 1, lastOneonOnIndex + 1).setBackgroundRGB(230, 160, 145);
-      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue("Overdue");
+      data.getCell(rowIndex + 1, lastOneonOneThreshold + 1).setValue(OVERDUE);
+    }
+  }
+
+  // Process meetings for the next six months to see if a meeting has been set up yet
+  var futureDate = new Date(now.getTime() + (180 * 24 * 60 * 60 * 1000));
+  var futureEvents = CalendarApp.getDefaultCalendar().getEvents(now, futureDate);
+
+  // Go through all the future events
+  for (var eventId = 0; eventId < futureEvents.length; eventId++) {
+    var currentEvent = futureEvents[eventId];
+    // Get the guests
+    var guests = currentEvent.getGuestList(true);
+    if (guests.length !== 2) {
+      // This isn't a 1:1 if there aren't exactly 2 guests
+      continue;
+    }
+
+    // Remove owner 
+    for (var guestId = 0; guestId < guests.length; guestId++) {
+      if (guests[guestId].getEmail().toLowerCase() == emailOfCalendarOwner) {
+        guests.splice(guestId, 1);
+      }
+    }
+ 
+    // Save off the date of the event
+    var meetingDate = currentEvent.getStartTime();
+    var meetingTitle = currentEvent.getTitle();
+ 
+    // Loop through each guest
+    for (var guestId = 0; guestId < guests.length; guestId++) {
+      var guestEmail = guests[guestId].getEmail().toLowerCase();
+      var guestStatus = guests[guestId].getGuestStatus();
+     
+      // Get the row corresponding to this guest
+      var guestEmailRow = rowDictionaryByEmail[guestEmail];
+      if (guestEmailRow == null) {
+        // if the guest isn't someone we are comparing in our 1:1 sheet we just move to the next
+        continue;
+      }
+     
+      // If we have a row, let's get the currently stored last meeting dates
+      var currentNextOneonOne = guestEmailRow[nextOneonOneIndex];
+      // Process 1:1, which means only 1 guest
+      if (currentNextOneonOne == null || currentNextOneonOne == "" || currentNextOneonOne > meetingDate) {
+          guestEmailRow[nextOneonOneIndex] = meetingDate;
+      }
+    }
+  }
+
+  // Commit updates to the sheet
+  data.setValues(values);
+
+  // Iterate through Close and Overdue to see if a future one-on-one is scheduled within cadence timeframe, then mark Good
+  for (const [email, row] of Object.entries(rowDictionaryByEmail)) {
+    if (row[lastOneonOneThreshold] == CLOSE || row[lastOneonOneThreshold] == OVERDUE) {
+      if (row[nextOneonOneIndex] !== null && row[nextOneonOneIndex] !== "") {
+        // There is a next one-on-one, check that it's within the cadence window
+        var cadence = row[cadenceIndex];
+        var totalPeriodFromToday = cadenceDictionary[cadence][0];
+        var lastDateOfFuturePeriod = new Date(now.getTime() + (totalPeriodFromToday * 24 * 60 * 60 * 1000));
+        if (row[nextOneonOneIndex] < lastDateOfFuturePeriod) {
+          data.getCell(row.rowIndex + 1, lastOneonOneThreshold + 1).setValue(GOOD);
+        }
+      }
     }
   }
  
